b2574bcec436ed3b4ad23fe27a321566bea268a9
Tom Nieuwland <thomasnieuwland19@gmail.com>
2019-10-16 23:56:19 +1100

    Initial commit

---
+++ src/main/java/com/tomnieuwland/beginnings/FizzBuzz.java
@@ -0,0 +1,23 @@
+package com.tomnieuwland.beginnings;
+
+// Fizzbuzz with no user input
+public class FizzBuzz {
+    public static void main(String[] args) {
+        int start = 1;
+        int end = 100;
+
+        for (int i = start; i <= end; i++){
+            String result = "";
+
+            if (i % 3 == 0){
+                result += "Fizz";
+            }
+
+            if (i % 5 == 0){
+                result += "Buzz";
+            }
+
+            System.out.printf("%d: %s%n", i, result);
+        }
+    }
+}
---
+++ src/main/java/com/tomnieuwland/beginnings/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.tomnieuwland.beginnings;
+
+// The humble hello world
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! This is Java!");
+    }
+}
