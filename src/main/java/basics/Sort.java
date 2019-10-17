a79205c3ac5f435afe73034706876fc774645845
Tyson Elliot Hooker <17778102+LuckyDucko@users.noreply.github.com>
2019-10-17 13:01:09 +1100

    New Sort method + Sort Checker
    Thought i would add another Sorting algorithm for you to check out. Very useful/10

--- src/main/java/basics/Sort.java
+++ src/main/java/basics/Sort.java
@@ -23,4 +23,26 @@ public class Sort {
         } while (!sorted);
         return arr;
     }
+    
+    public static int[] bogoSort(int[] arr){
+        do{
+            System.out.println("and another one");
+            for(int i = 0; i < arr.length -1; i++){
+                int random1 = (int)(Math.random() * arr.length);
+                int random2 = (int)(Math.random() * arr.length);
+                int temp = arr[random1];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }while (!isSorted(arr));
+        return arr;
+    }
+    
+    public static boolean isSorted(int[] arr){
+        for(int i = 0; i < arr.length -1; i++){
+            if(i != arr.length -1 && arr[i] > arr[i+1]) 
+            return false;
+        }
+        return true;
+    }
 }
